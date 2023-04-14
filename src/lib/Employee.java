6c333369e78c79b471ccd7cda950cbd9f0aa22f3
jeksilaen <88269870+jeksilaen@users.noreply.github.com>
2023-04-14 15:09:43 +0700

    Menghapus parameter constructor employee

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -13,20 +13,10 @@ public class Employee {
 	
 	private int monthWorkingInYear;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
-		this.employeeId = employeeId;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.idNumber = idNumber;
-		this.address = address;
-		this.yearJoined = yearJoined;
-		this.monthJoined = monthJoined;
-		this.dayJoined = dayJoined;
-		this.isForeigner = isForeigner;
-		this.gender = gender;
-		
-		childNames = new LinkedList<String>();
-		childIdNumbers = new LinkedList<String>();
+	public Employee(Personal personalInfo, Employment employmentInfo, Family familyInfo) {
+		this.personal = personalInfo;
+		this.employment = employmentInfo;
+		this.family = familyInfo;
 	}
 	
 	public int getAnnualIncomeTax() {
