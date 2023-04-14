c909d641f1d071dea9a466812f1738ca56cb7f2f
jeksilaen <88269870+jeksilaen@users.noreply.github.com>
2023-04-14 16:03:34 +0700

    Memindahkan constant menjadi global variable

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -1,16 +1,16 @@
 package lib;
 
 public class TaxFunction {
-	
+	final static int deductibleA = 54000000;
+	final static int deductibleB = 4500000;
+	final static int deductibleC = 1500000;
+	final static double roundable = 0.05;
+
 	public static int calculateTax(Employment employment, Family family, int numberOfMonthWorking) {
 		
 		int tax = 0;
 		int numberOfChildren = family.getChildIdNumbers();
-
-		final int deductibleA = 54000000;
-		final int deductibleB = 4500000;
-		final int deductibleC = 1500000;
-		final double roundable = 0.05;
+		boolean isMarried = family.getSpouseIdNumber() == "";
 		
 		if (numberOfMonthWorking > 12) {
 			System.err.println("More than 12 month working per year");
@@ -21,7 +21,7 @@ public class TaxFunction {
 	
 		
 		if (isMarried) {
-			tax = (int) Math.round(roundable * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (deductibleA + deductibleB + (numberOfChildren * deductibleC))));
+			tax = (int) Math.round(roundable * (((employment.getMonthlySalary() + employment.getOtherMonthlyIncome()) * numberOfMonthWorking) - employment.getAnnualDeductible() - (deductibleA + deductibleB + (numberOfChildren * deductibleC))));
 		}else {
 			tax = (int) Math.round(roundable * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - deductibleA));
 		}
